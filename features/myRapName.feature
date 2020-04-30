bc84ab3266867aad2ae2dd0555962a5b487e8687
sethwinters <swinte12@gmail.com>
2020-04-30 12:52:05 -0600

    Got all tests running, validated that 9 Scenarios are passing and made sure that message prints to STDOUT to validate test

--- features/myRapName.feature
+++ features/myRapName.feature
@@ -1,5 +1,6 @@
 Feature: MyRapName page displays Rap Names in ordered list
 
+
   Scenario: Validate that I am on the MyRapName.com homepage
     Given   I am on the "MyRapName.com" page
     Then    I see a message: "Enter your name to begin."
@@ -13,22 +14,22 @@ Feature: MyRapName page displays Rap Names in ordered list
     Given   I am on the "MyRapName.com" page
     When    I see an input field for "lastinitial"
     And     I can select and enter the text: "A" into the "lastinitial" input field
-    Then    I click the button "Suggest Male Rap Name"
+    Then    I click the button "Suggest Male Rap Name" "1" time(s)
     And     I see a message: "You must enter your first name."
 
   Scenario: Validate that First Name input field must not be empty when suggesting female names
     Given   I am on the "MyRapName.com" page
     When    I see an input field for "lastinitial"
     And     I can select and enter the text: "A" into the "lastinitial" input field
-    Then    I click the button "Suggest Female Rap Name"
+    Then    I click the button "Suggest Female Rap Name" "1" time(s)
     And     I see a message: "You must enter your first name."
 
   Scenario: Validate that input field processes input data entered into the field for male names
     Given   I am on the "MyRapName.com" page
     When    I see an input field for "firstname"
     And     I can select and enter the text: "firstName" into the "firstname" input field
-    And     I click the button "Suggest Male Rap Name"
-    Then    I see results listed in the "Your rap name is:" table
+    And     I click the button "Suggest Male Rap Name" "1" time(s)
+    Then    I see "1" result(s) listed in the "Your rap name is:" table
     And     I do not see a message: "You must enter your first name."
     And     I do not see a message: "Enter your name to begin."
 
@@ -36,22 +37,39 @@ Feature: MyRapName page displays Rap Names in ordered list
     Given   I am on the "MyRapName.com" page
     When    I see an input field for "firstname"
     And     I can select and enter the text: "firstName" into the "firstname" input field
-    And     I click the button "Suggest Female Rap Name"
-    Then    I see results listed in the "Your rap name is:" table
+    And     I click the button "Suggest Female Rap Name" "1" time(s)
+    Then    I see "1" result(s) listed in the "Your rap name is:" table
     And     I do not see a message: "You must enter your first name."
     And     I do not see a message: "Enter your name to begin."
-#
-#  Scenario: Validate 1 rap name is prepended for male first name, nickname no last initial
-#    Given   I am on the "MyRapName.com" page
-#    When    I do enter a first name
-#    And     I do not enter a last initial
-#    And     I do check "Use Nickname" checkbox
-#    Then    I see a new rap name at the top of the list of rap names
-#
-#  Scenario: Validate 2 rap names are prepended for female first name, last initial no nickname
-#    Given   I am on the "MyRapName.com" page
-#    When    I do enter a first name
-#    And     I do enter a last initial
-#    And     I do not check "Use Nickname" checkbox
-#    Then    I do see 2 new rap names
-#    And     The most recent rap name is at the top of the list
+
+  Scenario: Validate 1 rap name is prepended for male first name, with nickname no last initial
+    Given   I am on the "MyRapName.com" page
+    And     I see a message: "Enter your name to begin."
+    When    I see an input field for "firstname"
+    And     I can select and enter the text: "BigMaleRapperMan" into the "firstname" input field
+    And     I see an input field for "lastinitial"
+    And     I can select and enter the text: "" into the "lastinitial" input field
+    And     I can see the "Use Nickname:" checkbox and it should be checked = "true"
+    And     I click the button "Suggest Male Rap Name" "1" time(s)
+    Then    I see "1" result(s) listed in the "Your rap name is:" table
+
+  Scenario: Validate 2 rap names are prepended for female first name, last initial no nickname
+    Given   I am on the "MyRapName.com" page
+    And     I see a message: "Enter your name to begin."
+    When    I see an input field for "firstname"
+    And     I can select and enter the text: "RoughToughFemaleTesterWoman" into the "firstname" input field
+    And     I see an input field for "lastinitial"
+    And     I can select and enter the text: "Q" into the "lastinitial" input field
+    And     I can see the "Use Nickname:" checkbox and it should be checked = "false"
+    And     I click the button "Suggest Female Rap Name" "3" time(s)
+    Then    I see "3" result(s) listed in the "Your rap name is:" table
+
+  Scenario: Validate each time a new rap name is suggested it is prepended to the list and old suggestions move down
+    Given   I am on the "MyRapName.com" page
+    And     I see a message: "Enter your name to begin."
+    When    I see an input field for "firstname"
+    And     I can select and enter the text: "RepeatedRapperNamer" into the "firstname" input field
+    And     I see an input field for "lastinitial"
+    And     I can select and enter the text: "Z" into the "lastinitial" input field
+    And     I can see the "Use Nickname:" checkbox and it should be checked = "false"
+    Then    I click the button "Suggest Female Rap Name" "6" times and after each click I watch previous suggestions move down the "Your rap name is:" table
--- features/step_definitions/myRapName_steps.rb
+++ features/step_definitions/myRapName_steps.rb
@@ -1,57 +1,131 @@
+require 'logger'
+# TODO use Logs for debugging rather than puts - create logging config and print out debug logs to logfile
+# logger = Logger.new('logs/logfile.log')
+# logger.level = Logger::WARN
+# logger.warn("testing log file")
+
 ## Custom Methods
 
 Capybara.add_selector(:xpath) do
-  xpath {|arg| "//#{arg}"}
+  xpath { |arg| "//#{arg}" }
 end
 
+def clickonceandgetname(suggest_button, results_table, num)
+  page.click_on(suggest_button)
+  a = page.find(:xpath, "strong[contains(text(),'" + results_table + "')]/../following-sibling::div/table/tbody//tr[#{num}]").text
+  return a
+end
 
 ## Given Section ##
 
-Given /I am on the "(.*?)" page/ do |string|
-  if page.find(:xpath, "h1[contains(text(),'" + string + "')]")
-    puts('xpath on page matching: '+ string)
-  end
+Given /I am on the "(.*?)" page/ do |page_name|
+  page.find(:xpath, "h1[contains(text(),'" + page_name + "')]")
+  puts('xpath on page matching: ' + page_name)
 end
-
-
-
 
 ## When Section ##
 
-When(/^I see an input field for "(.*?)"$/) do |string|
-  if page.find(:xpath, "input[@name='" + string + "']")
-    puts string + ' input field is on the page'
-  end
+When(/^I see an input field for "(.*?)"$/) do |first_name|
+  page.find(:xpath, "input[@name='" + first_name + "']")
+  puts first_name + ' input field is on the page'
 end
-
-
-
 
 ## Then Section ##
 
-Then /I see a message: "(.*?)"/ do |string|
-  if page.find('h1', text: string)
-    puts 'The "' + string + '" text is visible on the screen'
+Then /I see a message: "(.*?)"/ do |alert_message|
+  page.find('h1', text: alert_message)
+  puts 'The "' + alert_message + '" text is visible on the screen'
+end
+
+Then(/^I can select and enter the text: "([^"]*)" into the "(.*?)" input field$/) do |actual_name, first_name|
+  page.fill_in(first_name, with: actual_name)
+  puts 'I can enter the text: "' + actual_name + '" into the "' + first_name + '" input field'
+end
+
+Then(/^I click the button "([^"]*)" "([^"]*)" time\(s\)$/) do |suggest_button, num_of_clicks|
+  num = num_of_clicks.to_i
+  if num.zero?
+    puts 'The button is not clicked at all'
+  elsif num == 1
+    page.click_on(suggest_button)
+    puts 'I clicked the "' + suggest_button + '" button'
+  else
+    x = 1
+    while num.positive?
+      page.click_on(suggest_button)
+      puts "I clicked the '" + suggest_button + "' button times #{x} times"
+      x += 1
+      num -= 1
+    end
   end
 end
 
-Then(/^I can select and enter the text: "([^"]*)" into the "(.*?)" input field$/) do |arg, string|
-  if page.fill_in(string, with: arg)
-    puts 'I can enter the text: "' + arg + '" into the "' + string + '" input field'
+Then(/^I see "([^"]*)" result\(s\) listed in the "([^"]*)" table$/) do |num_of_results, results_table|
+  num = num_of_results.to_i
+  if num == 1
+    a = page.find(:xpath, "strong[contains(text(),'" + results_table + "')]/../following-sibling::div/table/tbody//tr[#{num_of_results}]").text
+    puts 'The single result in the table is: ' + a
+  elsif num > 1
+    puts 'results are printed from bottom up - oldest to most recent'
+    while num.positive?
+      a = page.find(:xpath, "strong[contains(text(),'" + results_table + "')]/../following-sibling::div/table/tbody//tr[#{num}]").text
+      puts 'The number ' + num.to_s + ' result in the table is: ' + a
+      num -= 1
+    end
+  else
+    puts 'There must be more than 1 result in the table to validate'
   end
 end
 
-Then(/^I click the button "([^"]*)"$/) do |string|
-  if page.click_on(string)
-    puts 'I clicked the "' + string + '" button'
+Then(/^I do not see a message: "([^"]*)"$/) do |alert_message|
+  if page.assert_no_selector('h1', text: alert_message)
+    puts 'The message: "' + alert_message + '" is not on the screen'
   end
 end
 
-Then(/^I see results listed in the "([^"]*)" table$/) do |string|
-  a = page.find(:xpath, "strong[contains(text(),'" + string + "')]/../following-sibling::div/table").text
-  puts a
+Then(/^I can see the "([^"]*)" checkbox and it should be checked = "([^"]*)"$/) do |check_box_name, should_be_checked|
+  is_checked = page.find(:xpath, "h2[input][contains(text(),'" + check_box_name + "')]/input[3]").checked?
+  a = is_checked.to_s
+  b = check_box_name.to_s
+  c = should_be_checked.to_s
+  if is_checked == should_be_checked
+    puts 'The "' + b + '" checkbox is checked: "' + a + '" which matches what it should be: "' + c + '"'
+  else
+    puts 'The "' + b + '" checkbox is checked: "' + a + '" does not match what it should be: "' + c + '"'
+    find('input[type="checkbox"]').click
+    is_now_checked = page.find(:xpath, "h2[input][contains(text(),'" + b + "')]/input[3]").checked?
+    d = is_now_checked.to_s
+    puts 'The "' + b + '" checkbox is now checked: "' + d + '" which matches what it should be: "' + c + '"'
+  end
 end
 
-Then(/^I do not see a message: "([^"]*)"$/) do |string|
-  puts 'The message: "' + string + '" is not on the screen' if page.assert_no_selector('h1', text: string)
+# TODO create common methods that can replace duplicate code
+Then(/^I click the button "([^"]*)" "([^"]*)" times and after each click I watch previous suggestions move down the "([^"]*)" table$/) do |suggest_button,num_of_clicks,results_table|
+  num = num_of_clicks.to_i
+  suggestions = []
+  if num == 1
+    a = clickonceandgetname(suggest_button, results_table, num)
+    puts 'The single result in the table is: ' + a
+  end
+  if num > 1
+    x = 1
+    # this gets the name of the item at the top of the list and adds it to an arrayin the reverse order
+    # so that after 6 items are added, the oldest is element[0] and the newest is element[5]
+    while num.positive?
+      a = clickonceandgetname(suggest_button, results_table, x)
+      num -= 1
+      suggestions << a
+    end
+  end
+
+  x = num_of_clicks.to_i
+  suggestions.each do |suggestion|
+    suggestion_list = page.find(:xpath, "strong[contains(text(),'" + results_table + "')]/../following-sibling::div/table/tbody//tr[#{x}]").text
+    if suggestion == suggestion_list
+      puts 'list matches suggestion array'
+      x -= 1
+    else
+      puts 'list doesn\'t match'
+    end
+  end
 end
